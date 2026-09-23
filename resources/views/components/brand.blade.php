707cdfddaef36a5a1c1cdd2419da1b29c9464e9b
DarshanKondekar <darshankondekar01@gmail.com>
2026-09-23 12:10:23 +0530

    Updated files

--- resources/views/components/brand.blade.php
+++ resources/views/components/brand.blade.php
@@ -2,5 +2,5 @@
 
 <a href="{{ auth()->check() ? route('dashboard') : url('/') }}" class="brand {{ $light ? 'brand--light' : '' }}" aria-label="Proflect home">
     <img class="brand__logo" src="{{ asset('images/proflect-logo.webp') }}" alt="" aria-hidden="true">
-    <span class="brand__word">PROFLECT</span>
+    <?php /*<span class="brand__word">PROFLECT</span> */?>
 </a>
